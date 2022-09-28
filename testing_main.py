556eccf3362054c9fb275023a25c1a8b1d7bb9f2
Christoph Rohnert <Christoph.Rohnert@t-online.de>
2022-09-28 23:26:33 +0200

    Add Test to ensure restricted access to endpoints

--- testing_main.py
+++ testing_main.py
@@ -6,19 +6,20 @@ client = TestClient(app)
 
 def test_read_main():
     response = client.get("/")
-    assert response.status_code == 202
+    assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
 
-# def test_read_register():
-#     # response = client.post(
-#     #     "/register",
-#     #     headers={"X-Token": "coneofsilence"},
-#     #     json={
-#     #         "email": "user@example.com",
-#     #         "password": "string",
-#     #         "fullname": "string",
-#     #         "is_employee": True
-#     #         },
+def test_get_all_users():
+    response = client.get("/users")
+    assert response.json() == {"detail": "Not authenticated"}
+    assert response.status_code == 401
 
-#     # )
-#     assert 200 == 200
+def test_get_user_by_email():
+    response = client.get("/user_by_mail")
+    assert response.json() == {"detail": "Not authenticated"}
+    assert response.status_code == 401
+
+def test_get_all_books():
+    response = client.get("/books")
+    assert response.json() == {"detail": "Not authenticated"}
+    assert response.status_code == 401
