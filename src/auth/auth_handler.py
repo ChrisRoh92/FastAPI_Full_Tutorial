905c5721a7647f8a15e59daf5237f3a4ee6a6121
Christoph Rohnert <Christoph.Rohnert@t-online.de>
2023-01-02 22:29:32 +0100

    expire in json to exp

--- src/auth/auth_handler.py
+++ src/auth/auth_handler.py
@@ -18,7 +18,7 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     else:
         expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     
-    to_encode.update({"expire":expire})
+    to_encode.update({"exp":expire})
     ## create access token:
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
